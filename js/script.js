fc188a1d713a5f4fbe63f9ad1fb4b7bbc8738bae
Sophie Student <83235604+SophieStudent@users.noreply.github.com>
2022-04-26 18:12:34 +0000

    Updates

--- js/script.js
+++ js/script.js
@@ -16,20 +16,23 @@ function displayGreeting () {
 	// get user input
 	let hour = parseInt(document.getElementById('hour').value)
 
-
-  	// if hour is before noon, display "Good morning"
+  	// if hour is before noon, display "Good morning!"
 	if (hour < 12) {
 		greeting = "Good morning!"
 	} 
-	// otherwise, if hour is before 6pm, display "Good afternoon"	
+	// otherwise, if hour is before 6pm, display "Good afternoon:)"	
 	else if (hour < 18) {
 		greeting = "Good afternoon:)"
 	}
-	// otherwise, it must be evening
-	else {
+	// otherwise, if hour is before midnight, display "Good evening.."
+	else if (hour < 24) {
 		greeting = "Good evening..."
 	}
+	// otherwise, they did not enter a valid hour
+	else {
+		greeting = "Please enter a valid hour."
+	}
 
-  // display the results
+  // display the greeting
   document.getElementById('greeting').innerHTML = greeting
 }
